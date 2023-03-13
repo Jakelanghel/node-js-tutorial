ac82e910107e93b669f6878655385e3ddf7b5ab2
Jake <jacobtlanghel@gmail.com>
2023-03-13 13:28:10 -0400

    post with JS example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
+const data = require("./data");
+const people = data.people;
 
+// static assets
 app.use(express.static("./methods-public"));
-
+// parse form data
 app.use(express.urlencoded({ extended: false }));
-app.get("'api'people", (req, res) => {
+// parse json
+app.use(express.json());
+
+app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide a valid name!" });
+  }
+
+  res.status(201).send({ success: true, person: name });
 });
 
 app.post("/login", (req, res) => {
