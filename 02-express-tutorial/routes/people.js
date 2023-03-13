f0e9cc71614777e9a1ae2a62f4a55c8864a1b39c
Jake <jacobtlanghel@gmail.com>
2023-03-13 14:57:06 -0400

    second method

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -8,14 +8,16 @@ const {
   deletePerson,
 } = require("../controllers/people");
 
-router.get("/", getPeople);
+// router.get("/", getPeople);
+// router.post("/", createPerson);
+// router.post("/postman", createPersonPostman);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
 
-router.post("/", createPerson);
-
-router.post("/postman", createPersonPostman);
-
-router.put("/:id", updatePerson);
-
-router.delete("/:id", deletePerson);
+// second option on how to set them up..
+// they both do the same thing so its rly up t preference
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
