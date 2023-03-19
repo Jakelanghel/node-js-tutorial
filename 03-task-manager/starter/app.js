76aefb6137abbe31943ae7f4e410b2cd0c4339ba
Jake <jacobtlanghel@gmail.com>
2023-03-19 00:38:53 -0400

    finished getAllTasks getTask and updateTask

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
+
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 // middleware
 app.use(express.json());
+app.use(express.static("./public"));
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task manager app");
-});
-
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
